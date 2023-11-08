1467078021fdbb5eabaa3f53f0bbd9f21857a92f
dgant <mickeylol98@gmail.com>
2023-11-08 17:37:30 -0500

    price first machine learning model

--- bestsellers.py
+++ bestsellers.py
@@ -1,12 +1,29 @@
+from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 
-df= pd.read_csv("E:\Destinee's\Data\AmazonBestSeller\\archive\\amazonbestsellers.csv")
 
-print(df.head())
-print(df.shape)
+bestsellers_data= pd.read_csv("E:/Destinee's/Data/AmazonBestSeller/archive/amazonbestsellers.csv")
+
+print(bestsellers_data.head())
+print(bestsellers_data.shape)
 
 #Clean Data
-df.drop_duplicates(inplace = True)
+bestsellers_data.drop_duplicates(inplace = True)
 
-df.rename(columns={"Name":"Title","Year":"Publication Year", "User Rating":"Rating"}, inplace = True)
-df["Price"]=df["Price"].astype(float)
+bestsellers_data.rename(columns={"Name":"Title","Year":"Publication Year", "User Rating":"Rating"}, inplace = True)
+bestsellers_data["Price"]=bestsellers_data["Price"].astype(float)
+
+# Set y 
+y = bestsellers_data.Price
+#Create List of features
+book_features = ["Rating","Reviews"]
+X = bestsellers_data[book_features]
+
+#test
+print(X.describe())
+
+#price modeling
+price_model = DecisionTreeRegressor(random_state=1)
+print(price_model.fit(X,y))
+price_predictions = price_model.predict(X)
+print(price_predictions)
