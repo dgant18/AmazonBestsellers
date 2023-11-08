41e615a969a7b8f9a6fdf47ffaf9f4be6a9cfa04
dgant <mickeylol98@gmail.com>
2023-11-08 12:05:39 -0500

    intial commit

--- bestsellers.py
+++ bestsellers.py
@@ -4,3 +4,9 @@ df= pd.read_csv("E:\Destinee's\Data\AmazonBestSeller\\archive\\amazonbestsellers
 
 print(df.head())
 print(df.shape)
+
+#Clean Data
+df.drop_duplicates(inplace = True)
+
+df.rename(columns={"Name":"Title","Year":"Publication Year", "User Rating":"Rating"}, inplace = True)
+df["Price"]=df["Price"].astype(float)
